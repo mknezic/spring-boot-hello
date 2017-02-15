62050037ece34766bcad5b3ce49bc9421b69722f
mknezic <mknezic@croz.net>
2017-02-15 09:51:00 +0100

    boot liquibase and cloud, dao + service

--- src/main/resources/db/scripts/00001.sql
+++ src/main/resources/db/scripts/00001.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
---changeset me:0001.1
+--changeset me:0001.1 context=all
 CREATE TABLE template
 (
     id serial,
@@ -9,7 +9,7 @@ CREATE TABLE template
 --rollback DROP TABLE template;
 
 --liquibase formatted sql
---changeset me:0001.2
+--changeset me:0001.2 context=all
 insert into template(text) values ('Hello, %s!');
 insert into template(text) values ('Zdravstvujte, %s!');
 insert into template(text) values ('Bok, %s!');
